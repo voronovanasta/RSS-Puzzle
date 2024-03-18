65931002963ef3815c7b05ab0575c4399113ac50
voronovanasta <110159859+voronovanasta@users.noreply.github.com>
2024-03-18 07:03:21 +0300

    Feat/rss pz 34 setup project files (#34)
    * feat:RSS-PZ-34 Add configure files for eslint, webpack, ts, prettier
    
    * feat: RSS-PZ-34 setup husky
    
    * fix: RSS-PZ-34 add regexp, fix husky files
    
    * fix: RSS-PZ-34 fix deploy script
    
    ---------
    
    Co-authored-by: anastasiya voronova <buchneva.anastasya92@gmail.comm>

---
+++ rss-puzzle/webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin')
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/index'),
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            { test: /\.ts$/i, use: 'ts-loader' },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+              },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+          new TerserPlugin(),
+          new CssMinimizerPlugin(),
+          new HtmlWebpackPlugin({
+            template: './src/index.html',
+            minify: {
+              removeAttributeQuotes: true,
+              collapseWhitespace: true,
+              removeComments: true,
+            },
+          }),
+        ],
+      },
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 8000,
+        hot: true,
+        open: true,
+        historyApiFallback: true, // for SPA
+      },
+    resolve: {
+        extensions: ['.ts','.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, './dist'),
+        clean: true,
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new ESLintPlugin({ extensions: 'ts' }),
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
