a475a5ce9a6020973fc3119a561f79261bfdc17a
voronovanasta <110159859+voronovanasta@users.noreply.github.com>
2024-03-21 10:13:12 +0300

    feat: RSS-PZ-07 implement design for start button (#42)
    Co-authored-by: anastasiya voronova <buchneva.anastasya92@gmail.comm>

--- rss-puzzle/src/components/StartPage/StartPageComponent.ts
+++ rss-puzzle/src/components/StartPage/StartPageComponent.ts
@@ -5,7 +5,9 @@ const StartPageComponent = () => `<div class="start-wrapper">
     <p id="description-content" class="content-field">By clicking on words you collects phrases. Use drag and drop, tooltips in the menu  provides you with assistance.
     </p>
 </div>
-<a href="/game" class="button startBtn target-link" id = "game">Start Game</a>
+<a href="/game" class="button startBtn target-link" id = "game"><span class="material-symbols-outlined">
+play_circle
+</span><span>Play</span></a>
 <a href="/" class="logout target-link">
 <span class="material-symbols-outlined" id = "logoutBtn">logout</span>
 </a>
