f3dd47e0d78ed67862f43793a64d71ec24e49d1e
voronovanasta <110159859+voronovanasta@users.noreply.github.com>
2024-03-21 07:06:30 +0300

    fix: RSS-PZ-05 fix  design of start page (#40)
    Co-authored-by: anastasiya voronova <buchneva.anastasya92@gmail.comm>

--- rss-puzzle/src/components/StartPage/StartPageComponent.ts
+++ rss-puzzle/src/components/StartPage/StartPageComponent.ts
@@ -1,7 +1,8 @@
 const StartPageComponent = () => `<div class="start-wrapper">
-    <h1>Puzzle</h1>
+    <h3>English Puzzle</h3>
     <div id="description" class="description-field">
-    <p id="description-content" class="content-field"></p>
+    <p id="description-content" class="content-field">By clicking on words you collects phrases. Use drag and drop, tooltips in the menu  provides you with assistance.
+    </p>
 </div>
 <a href="/game" class="button startBtn target-link" id = "game">Start Game</a>
 <a href="/" class="logout target-link">
